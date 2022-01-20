ae11b9d81529a0b6d2ebf4cbebeeec7cf4be30b9
robinbdru <robinbeaudru@icloud.com>
2022-01-20 11:38:34 +0100

    Creation Upload Module + Upload Modal.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import NavBarTop from "./components/navigation/NavBarTop";
 import HeaderOneCenter from "./components/headers/HeaderOneCenter";
 import HeaderThreeCenter from "./components/headers/HeaderThreeCenter";
-import HeaderTwoLeft from "./components/headers/HeaderTwoLeft";
+import Upload from "./components/upload/Upload";
+import ModalUpload from "./components/modal/ModalUpload";
 
 function App() {
+	const [showModal, setShowModal] = useState(false);
+
+	function handleShowModal() {
+		setShowModal(!showModal);
+	}
+
 	return (
 		<div className="relative h-full App font-open-sans bg-slate-50">
+			{showModal ? (
+				<div className="fixed inset-0 z-10 flex flex-col items-center justify-center w-full h-full bg-slate-500 bg-opacity-30">
+					<ModalUpload handleShowModal={handleShowModal} />
+				</div>
+			) : null}
 			<NavBarTop />
 			<section className="bg-[url('./assets/images/gradient.png')] bg-cover w-full bg-no-repeat bg-center">
-				<div className="flex flex-col items-center justify-center w-full max-w-3xl min-h-screen pt-32 mx-auto md:pt-0">
+				<div className="flex flex-col items-center justify-center w-full min-h-screen pt-32 mx-auto mt-12 max-w-7xl md:pt-0">
 					<HeaderOneCenter text="Mano Lens helps you find products in our catalog, from inspiration images." />
 					<div className="flex flex-col w-full max-w-2xl mt-12 gap-y-8">
 						<HeaderThreeCenter text="This tool is made for both consumers and professionals customers of ManoMano." />
-						<HeaderThreeCenter text="Scroll this page to discover it. Enjoy!" />
+						<HeaderThreeCenter text="Start by uploading your image !" />
 					</div>
-				</div>
-			</section>
-			<section className="flex flex-col items-center w-full px-4 pt-32 mx-auto gap-y-8 gap-x-12 lg:flex-row lg:px-12 pb-36 md:pt-0">
-				<div className="w-full max-w-2xl lg:pr-12">
-					<HeaderTwoLeft text="Collect inspirational images on the web and easily find references in the catalogue." />
-				</div>
-				<div className="w-full h-auto px-16 py-12 bg-slate-200 bg-opacity-40 rounded-xl">
-					test
-				</div>
-			</section>
-			<section className="flex flex-col items-center w-full px-4 pt-32 mx-auto gap-y-8 gap-x-12 lg:flex-row lg:px-12 pb-36 md:pt-0">
-				<div className="w-full max-w-2xl lg:pr-12">
-					<HeaderTwoLeft text="Professionals save time in finding fashionable references from social networks like Pinterest." />
-				</div>
-				<div className="w-full h-auto px-16 py-12 bg-slate-200 bg-opacity-40 rounded-xl">
-					test
+					<Upload handleShowModal={handleShowModal} />
 				</div>
 			</section>
 		</div>
