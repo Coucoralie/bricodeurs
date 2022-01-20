13b0473a28ae0262834cd8782941e2836b638481
NanakiMathieu <mathieu.nanaki@gmail.com>
2022-01-20 11:28:18 +0100

    add carousel component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "../src/components/Header";
+import Carousel from "./components/Carousel/Carousel";
+import { CarouselData } from './components/Carousel/CarouselData';
+
 
 function App() {
 	return (
 		<div className="App font-open-sans">
 			<div className="flex flex-col items-center justify-center h-screen bg-gray-50">
-				<Header text="ManoMano & Les Bricodeurs" />
+				<Carousel slides={CarouselData}/>
 			</div>
 		</div>
 	);
