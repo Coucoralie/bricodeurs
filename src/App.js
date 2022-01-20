6f9f44ba16b5b0f16e939ab84112b0f03400374a
NanakiMathieu <mathieu.nanaki@gmail.com>
2022-01-20 14:58:53 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import NavBarTop from "./components/navigation/NavBarTop";
+import FicheThemes from "./components/FicheThemes/FicheThemes"
 
 function App() {
 	return (
+		<div>
 		<div className="relative h-full App font-open-sans bg-slate-50">
 			<NavBarTop />
 			<Home />
+		</div>
+
+		<Routes>
+			<Route path='/' element={<Home />}/>
+			<Route path='/themes' element={<FicheThemes />}/>
+			<Route path='/' element={<Home />}/>
+		</Routes>
 		</div>
 	);
 }
