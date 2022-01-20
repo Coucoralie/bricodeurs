875b530afb10dd13e0fde6da879c8e4dd79b3b48
Coucoralie <c.cocourant@gmail.com>
2022-01-20 12:04:06 +0100

    adding images

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+export { default as cuisine } from  "./cuisine.jpeg";
+export { default as cuisine2} from "./cuisine2.jpeg";
+export { default as cuisine3} from "./cuisine3.jpeg";
+export { default as cuisine4} from "./cuisine4.jpeg";
+export { default as cuisine5} from "./cuisine5.jpeg";
+export { default as cuisine6} from "./cuisine6.jpeg";
